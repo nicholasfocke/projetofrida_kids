8a523c0a4aea5c3ea97393f04d93982f2c8680c4
Kauê Cavalcante <Kauecavalcante4@gmail.com>
2024-10-22 21:48:21 -0300

    exclusao das funcoes de saudaçoes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,7 @@
-import { ReactNode, useEffect, useState } from 'react';
+import { ReactNode, useState } from 'react';
 import { FaInstagram, FaBars, FaTimes, FaSignOutAlt } from 'react-icons/fa'; 
-import { auth, firestore } from '../firebase/firebaseConfig';
+import { auth } from '../firebase/firebaseConfig';
 import { signOut } from 'firebase/auth';
-import { doc, getDoc } from 'firebase/firestore';
-import { onAuthStateChanged } from 'firebase/auth';
 import Image from 'next/image';
 import styles from './Layout.module.css';
 import Link from 'next/link';
@@ -14,50 +12,8 @@ type LayoutProps = {
 };
 
 const Layout = ({ children }: LayoutProps) => {
-  const [userName, setUserName] = useState('');
-  const [greeting, setGreeting] = useState('');
   const [menuOpen, setMenuOpen] = useState(false); // Controla se o menu está aberto
-  const [loading, setLoading] = useState(true); // Estado de carregamento
   const router = useRouter(); // Hook para redirecionamento
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      if (!userName) {
-        setUserName('Visitante'); // Define como "Visitante" após 10 segundos se o nome não for recuperado
-        setLoading(false);
-      }
-    }, 10000); // 10 segundos para exibir "Visitante"
-
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        // Pega o nome do usuário imediatamente após o login
-        const userDoc = doc(firestore, 'users', user.uid);
-        const docSnap = await getDoc(userDoc);
-        if (docSnap.exists()) {
-          const fullName = docSnap.data().nome || 'Usuário';
-          const firstName = fullName.split(' ')[0];
-          setUserName(firstName); // Atualiza o nome do usuário
-          clearTimeout(timeoutId); // Cancela o timeout de 10 segundos
-        }
-      }
-      setLoading(false); // Desativa o carregamento quando o nome for recuperado
-    });
-
-    // Definir saudação com base no horário atual
-    const hours = new Date().getHours();
-    if (hours >= 5 && hours < 12) {
-      setGreeting('Bom dia');
-    } else if (hours >= 12 && hours < 18) {
-      setGreeting('Boa tarde');
-    } else {
-      setGreeting('Boa noite');
-    }
-
-    return () => {
-      unsubscribe();
-      clearTimeout(timeoutId); // Limpa o timeout quando o componente desmontar
-    };
-  }, [userName]);
 
   // Função para alternar a abertura e fechamento do menu
   const toggleMenu = () => {
@@ -74,12 +30,7 @@ const Layout = ({ children }: LayoutProps) => {
     }
   };
 
-  
   const noHeaderRoutes = ['/login', '/register', '/esquecisenha'];
-
-  if (loading) {
-    return <div>Carregando...</div>; // Exibe "Carregando..." enquanto espera o nome ou timeout
-  }
 
   return (
     <div className={styles.layout}>
