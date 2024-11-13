aa80d3fb2ea46439021af7dc98d343eab1353f0e
nicholasfocke <nicholasfocke05@gmail.com>
2024-11-12 21:27:15 -0300

    Update index horario

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ const Index = () => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
   const [availableTimes, setAvailableTimes] = useState<string[]>([]);
   const [error, setError] = useState('');
-  const times = ['09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30'];
+  const times = ['08:30','09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30'];
 
   const router = useRouter(); 
 
