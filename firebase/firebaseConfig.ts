43f70548a2e82022f05d22a1b0dba65791b93e5d
nicholasfocke <nicholasfocke05@gmail.com>
2025-02-16 23:35:40 -0300

    Update

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCysxDkBAb4LERFRFPltOyMxGMbFH0IJ0A",
-  authDomain: "fridakids-a0f1f.firebaseapp.com",
-  projectId: "fridakids-a0f1f",
-  storageBucket: "fridakids-a0f1f.appspot.com",
-  messagingSenderId: "344032070158",
-  appId: "1:344032070158:web:de42cd3addf58f8f87ca5f",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Inicializa o Firebase apenas se ainda não foi inicializado
