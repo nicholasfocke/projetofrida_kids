426b0c842bfaf6ee67a0479a82f50faf8b8cb617
nicholasfocke <nicholasfocke05@gmail.com>
2025-02-17 01:10:10 -0300

    Update favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import { Html, Head, Main, NextScript } from 'next/document'
- 
+import { Html, Head, Main, NextScript } from 'next/document';
+
 export default function Document() {
   return (
-    <Html>
-      <Head />
+    <Html lang="pt-BR">
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+
+        {/* SEO e Meta Tags */}
+        <meta name="description" content="Frida Kids - Agendamentos para cortes e penteados infantis." />
+
+        {/* Google Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
