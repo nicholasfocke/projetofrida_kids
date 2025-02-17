70944408decb7652818aafa071a12be4e7700107
nicholasfocke <nicholasfocke05@gmail.com>
2025-02-17 15:34:38 -0300

    update zoom no celular

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,24 @@
 import { AuthProvider } from '../context/AuthContext';
 import '../styles/globals.css';
 import Layout from '../components/Layout';
-
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-    
-    <Layout>
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
-    </Layout>
+    <>
+      <Head>
+        {/* Corrige zoom inesperado no mobile e mantém responsividade */}
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <title>Frida Kids</title>
+        <meta name="description" content="Sistema de agendamentos Frida Kids." />
+      </Head>
+
+      <AuthProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthProvider>
+    </>
   );
 }
 
